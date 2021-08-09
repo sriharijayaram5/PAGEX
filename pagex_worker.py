c8dcc423964309df5af193d00e3a28971ce2210e
Sreehari Jayaram <Sriharijayaram5@gmail.com>
2021-08-09 10:27:45 +0200

    Fixed slow GUI startup

--- pagex_worker.py
+++ pagex_worker.py
@@ -913,7 +913,7 @@ def main(comp_0a, do_what_now, output, ff1, comp_1a, comp_2a, eflag, mfp, densit
 def run_gui():
     """Starts the program with a web brower based GUI for easy input and a help page.
     """
-    eel.init('web')
+    eel.init('web', allowed_extensions=['.html'])
     try:
         eel.start('landing2.4.html', size=(1024, 550), mode='chrome')
     except (SystemExit, MemoryError, KeyboardInterrupt):
